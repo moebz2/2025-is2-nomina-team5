7c7cb703ac56bda6818008c4f612e097806bf3aa
mbaez091fpuna <mbaez091@fpuna.edu.py>
2025-03-23 12:50:57 -0400

    fix cantidad por pagina

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
 
     public function index()
     {
-        $users = User::with('empleado.departamento')->paginate(1);
+        $users = User::with('empleado.departamento')->paginate(10);
         return view('users.index', compact('users'));
     }
 
